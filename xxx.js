b2c2f5f676dac6edc7b24ec035cc0cd7ccdb3f5d
Kadal Krenyes <44739068+kadalkrenyes@users.noreply.github.com>
2024-10-02 22:24:02 +0700

    Update xxx.js

--- xxx.js
+++ xxx.js
@@ -1,5 +1,27 @@
+document.addEventListener("DOMContentLoaded",function(){
+ var body=document.body;
+  setInterval(createStar,50);
+  function createStar(){
+    var right=Math.random()*500;
+    var top=Math.random()*screen.height;
+    var star=document.createElement("div");
+ star.classList.add("star")
+  body.appendChild(star);
+  setInterval(runStar,10);
+    star.style.top=top+"px";
+  function runStar(){
+    if(right>=screen.width){
+      star.remove();
+    }
+    right+=3;
+    star.style.right=right+"px";
+  }
+  } 
+})
+
+//   play audio code start
  const audioPlayer = document.querySelector(".audio-player");
-    const audio = new Audio("Gravity.mp3");
+    const audio = new Audio("https://farizal.web.id/Gravity.mp3");
     console.dir(audio);
 
     audio.addEventListener(
